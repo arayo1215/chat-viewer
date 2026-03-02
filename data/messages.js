dc50704283abf5cef9388b5deadb7c2f65d02218
arayo1215 <85500045+arayo1215@users.noreply.github.com>
2026-03-02 22:48:26 +0800

    Update message senders in messages.js

--- data/messages.js
+++ data/messages.js
@@ -1,16 +1,17 @@
 const messages = [
   {
     id: "m1",
-    sender: "me",  // "me" 或 "colleague"
+    sender: "kt",  // "me" 或 "colleague"
     text: "欸你昨天那個 API 是不是壞了",
     time: "22:14",
     pinned: false
   },
   {
     id: "m2",
-    sender: "colleague",
+    sender: "yc",
     text: "沒有吧，我本機是正常的",
     time: "22:15",
     pinned: true
   }
+
 ];
